12a20e68fef8d509e382dee6c1fb91a5f7420cc1
eunoia73 <soo97703@naver.com>
2025-07-06 15:36:45 +0900

    feat: #4 이벤트 핸들링

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,16 @@
-const Button = ({ children, text, color = "black" }) => { //구조분해할당으로 기본값 설정
+const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e); // 이벤트 객체
+    console.log(text);
+  };
+
+  // 구조분해할당으로 기본값 설정
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton} // 함수의 이름만 전달하면 됨. 이렇게 onClickButton() 함수의 실행 결과 말고
+      //   onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
