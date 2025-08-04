1bf3e1b5d3219b3ef88f934a771cec970fff582e
eunoia73 <soo97703@naver.com>
2025-08-04 09:47:21 +0900

    feat: #5 간단한 회원가입 state로 관리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+// import Bulb from "./components/Bulb";
+// import Counter from "./components/Counter";
+
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      {/* <Bulb /> */}
+      {/* <Counter /> */}
+
+      <Register />
     </>
   );
 }
