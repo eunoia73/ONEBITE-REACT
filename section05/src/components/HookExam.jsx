b626bc63105b38df2b4ef56228f419991ee67206
eunoia73 <soo97703@naver.com>
2025-08-04 22:56:16 +0900

    feat: #7 custom hook 모듈화

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
+import useInput from "../hooks/useInput";
 
 // 3가지 hook 관련된 팁
 // 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
 // 2. 조건부로 호출될 수는 없다
 // 3. 나만의 훅(Custom Hook)을 만들 수 있다
-
-function useInput() {
-  // 커스텀 훅
-  const [input, setInput] = useState("");
-
-  const onChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  return [input, onChange];
-}
 
 const HookExam = () => {
   const [input, onChange] = useInput(); //배열의 구조분해 할당
