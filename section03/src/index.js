6bfeac95775da8f3efaee5a2b87c0b1ca6336b22
eunoia73 <soo97703@naver.com>
2025-08-05 23:33:58 +0900

    feat: #8 randomColor 라이브러리 사용해보기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,10 +1,11 @@
 // ES6 모듈 방식으로 가져오기
 import mul, { add, sub } from "./math.js";
-// default로 내보낸 함수는 이름 변경 가능
 
-// CommonJS 방식으로 모듈을 가져오기
-// const { add, sub } = require("./math");
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
