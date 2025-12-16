0a2acc4847cc62eaa00c9c922f9a5a4787eb5229
eunoia73 <soo97703@naver.com>
2025-12-16 14:09:53 +0900

    feat: #15 Notfound 페이지 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -3,16 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
   return (
+    // Routes 태그 바깥에 있는 요소는 모든 페이지에 공통으로 적용된다.
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
       <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
     </Routes>
   );
 }
