7796074ce42121735f4460ca8825523bcca9c42f
eunoia73 <soo97703@naver.com>
2025-12-16 14:39:52 +0900

    feat: #15 동적 경로 설정

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  // 현재 브라우저에 명시한 url 파라미터를 가져오는 기능을 함
+  const params = useParams();
+  console.log(params);
+
+  return <div>{params.id}번 일기입니다!</div>;
 };
 
 export default Diary;
