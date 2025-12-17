b1f1addf88ca0b96670c44b80ca15b36db855191
eunoia73 <soo97703@naver.com>
2025-12-17 10:14:33 +0900

    feat: #18 mockData, reducer 생성

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,25 +1,38 @@
 import "./App.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
-
-import { getEmotionImage } from "./util/get-emotion-image";
-
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-function App() {
-  const nav = useNavigate(); //navigating 함수 저장
 
-  const onClickButton = () => {
-    nav("/new");
-  };
+//임시데이터로 data state에 저장될 일기 데이터의 객체 형식 지정
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
