ae1dd7589ac77f4d78d37d1fbc21e5b14c708f01
eunoia73 <soo97703@naver.com>
2025-12-18 12:36:51 +0900

    feat: #20 오늘의 날짜 구현

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,9 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
+import { useState } from "react";
+
+const emotionId = 1;
 
 const emotionList = [
   {
@@ -25,15 +28,58 @@ const emotionList = [
   },
 ];
 
-const emotionId = 1;
+const getStringedDate = (targetDate) => {
+  // 날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  // 한 자리 숫자는 형식에 맞지 않으므로 변환 필요
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
 
 const Editor = () => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const onChangeInput = (e) => {
+    let name = e.target.name; // 어떤 요소에 입력이 들어온건지
+    let value = e.target.value; // 입력된 값이 무엇인지?
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        {/* input 태그의 value값은 String 형식으로 전환해야 인식*/}
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
+
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
@@ -46,6 +92,7 @@ const Editor = () => {
           ))}
         </div>
       </section>
+
       <section className="content_section">
         <h4>오늘의 일기</h4>
         <textarea placeholder="오늘은 어땠나요?" />
