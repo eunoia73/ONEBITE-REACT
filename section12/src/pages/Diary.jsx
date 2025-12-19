6715811e1437b8e3e10e7f691a8f36f223a5f1f0
eunoia73 <soo97703@naver.com>
2025-12-19 11:30:34 +0900

    feat: #22 Diary 페이지 구조 및 스타일링 설정

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
 
 const Diary = () => {
   // 현재 브라우저에 명시한 url 파라미터를 가져오는 기능을 함
   const params = useParams();
-  console.log(params);
 
-  return <div>{params.id}번 일기입니다!</div>;
+  return (
+    <div>
+      <Header
+        title={"yyyy-mm-dd 기록"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+        rightChild={<Button text={"수정하기"} />}
+      />
+      <Viewer />
+    </div>
+  );
 };
 
 export default Diary;
